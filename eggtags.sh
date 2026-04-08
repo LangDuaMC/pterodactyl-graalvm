720ce203dfd40636b2b2fe2f438d1144a130f658
stdpi <git@stdpi.work>
2026-04-08 20:50:59 +0700

    change namespace to langduamc

--- eggtags.sh
+++ eggtags.sh
@@ -13,26 +13,26 @@ echo "22-JDK
 25-OC
 22-temurin
 23-OC
-23-temurin" | sed -re 's/(.*)$/GraalVM &|ghcr.io\/quantrieupcyt\/pterodactyl-graalvm:&/'
+23-temurin" | sed -re 's/(.*)$/GraalVM &|ghcr.io\/langduamc\/pterodactyl-graalvm:&/'
 
 # Copy this list into dashboard.acme.co/admin/nests/egg/<the egg ofc>
 echo "
 
-GraalVM 22-JDK|ghcr.io/quantrieupcyt/pterodactyl-graalvm:22-JDK
-GraalVM 17-JDK|ghcr.io/quantrieupcyt/pterodactyl-graalvm:17-JDK
-GraalVM 11-EE|ghcr.io/quantrieupcyt/pterodactyl-graalvm:11-EE
-GraalVM 11|ghcr.io/quantrieupcyt/pterodactyl-graalvm:11
-GraalVM 17|ghcr.io/quantrieupcyt/pterodactyl-graalvm:17
-GraalVM 21-JDK|ghcr.io/quantrieupcyt/pterodactyl-graalvm:21-JDK
-GraalVM 19|ghcr.io/quantrieupcyt/pterodactyl-graalvm:19
-GraalVM 17-EE|ghcr.io/quantrieupcyt/pterodactyl-graalvm:17-EE
-GraalVM 19-EE|ghcr.io/quantrieupcyt/pterodactyl-graalvm:19-EE
-GraalVM 20-JDK|ghcr.io/quantrieupcyt/pterodactyl-graalvm:20-JDK
-GraalVM 22-OC|ghcr.io/quantrieupcyt/pterodactyl-graalvm:22-OC
-GraalVM 25-OC|ghcr.io/quantrieupcyt/pterodactyl-graalvm:25-OC
-GraalVM 23-OC|ghcr.io/quantrieupcyt/pterodactyl-graalvm:23-OC
-Temurin 22|ghcr.io/quantrieupcyt/pterodactyl-graalvm:22-temurin
-Temurin 21|ghcr.io/quantrieupcyt/pterodactyl-graalvm:21-temurin
-Temurin 23|ghcr.io/quantrieupcyt/pterodactyl-graalvm:23-temurin
+GraalVM 22-JDK|ghcr.io/langduamc/pterodactyl-graalvm:22-JDK
+GraalVM 17-JDK|ghcr.io/langduamc/pterodactyl-graalvm:17-JDK
+GraalVM 11-EE|ghcr.io/langduamc/pterodactyl-graalvm:11-EE
+GraalVM 11|ghcr.io/langduamc/pterodactyl-graalvm:11
+GraalVM 17|ghcr.io/langduamc/pterodactyl-graalvm:17
+GraalVM 21-JDK|ghcr.io/langduamc/pterodactyl-graalvm:21-JDK
+GraalVM 19|ghcr.io/langduamc/pterodactyl-graalvm:19
+GraalVM 17-EE|ghcr.io/langduamc/pterodactyl-graalvm:17-EE
+GraalVM 19-EE|ghcr.io/langduamc/pterodactyl-graalvm:19-EE
+GraalVM 20-JDK|ghcr.io/langduamc/pterodactyl-graalvm:20-JDK
+GraalVM 22-OC|ghcr.io/langduamc/pterodactyl-graalvm:22-OC
+GraalVM 25-OC|ghcr.io/langduamc/pterodactyl-graalvm:25-OC
+GraalVM 23-OC|ghcr.io/langduamc/pterodactyl-graalvm:23-OC
+Temurin 22|ghcr.io/langduamc/pterodactyl-graalvm:22-temurin
+Temurin 21|ghcr.io/langduamc/pterodactyl-graalvm:21-temurin
+Temurin 23|ghcr.io/langduamc/pterodactyl-graalvm:23-temurin
 
 " > /dev/null
