af3b37d07c2980ff3d91ac1042cd04baed74b2dc
stdpi <git@stdpi.work>
2026-04-08 21:17:19 +0700

    make ghcr publishing generic

--- eggtags.sh
+++ eggtags.sh
@@ -1,3 +1,7 @@
+GHCR_NAMESPACE="${GHCR_NAMESPACE:-langduamc}"
+GHCR_IMAGE="${GHCR_IMAGE:-pterodactyl-graalvm}"
+IMAGE_REF="ghcr.io/${GHCR_NAMESPACE,,}/${GHCR_IMAGE,,}"
+
 echo "22-JDK
 17-JDK
 11-EE
@@ -13,26 +17,26 @@ echo "22-JDK
 25-OC
 22-temurin
 23-OC
-23-temurin" | sed -re 's/(.*)$/GraalVM &|ghcr.io\/langduamc\/pterodactyl-graalvm:&/'
+23-temurin" | sed -re "s~(.*)$~GraalVM \\1|${IMAGE_REF}:\\1~"
 
 # Copy this list into dashboard.acme.co/admin/nests/egg/<the egg ofc>
 echo "
 
-GraalVM 22-JDK|ghcr.io/langduamc/pterodactyl-graalvm:22-JDK
-GraalVM 17-JDK|ghcr.io/langduamc/pterodactyl-graalvm:17-JDK
-GraalVM 11-EE|ghcr.io/langduamc/pterodactyl-graalvm:11-EE
-GraalVM 11|ghcr.io/langduamc/pterodactyl-graalvm:11
-GraalVM 17|ghcr.io/langduamc/pterodactyl-graalvm:17
-GraalVM 21-JDK|ghcr.io/langduamc/pterodactyl-graalvm:21-JDK
-GraalVM 19|ghcr.io/langduamc/pterodactyl-graalvm:19
-GraalVM 17-EE|ghcr.io/langduamc/pterodactyl-graalvm:17-EE
-GraalVM 19-EE|ghcr.io/langduamc/pterodactyl-graalvm:19-EE
-GraalVM 20-JDK|ghcr.io/langduamc/pterodactyl-graalvm:20-JDK
-GraalVM 22-OC|ghcr.io/langduamc/pterodactyl-graalvm:22-OC
-GraalVM 25-OC|ghcr.io/langduamc/pterodactyl-graalvm:25-OC
-GraalVM 23-OC|ghcr.io/langduamc/pterodactyl-graalvm:23-OC
-Temurin 22|ghcr.io/langduamc/pterodactyl-graalvm:22-temurin
-Temurin 21|ghcr.io/langduamc/pterodactyl-graalvm:21-temurin
-Temurin 23|ghcr.io/langduamc/pterodactyl-graalvm:23-temurin
+GraalVM 22-JDK|${IMAGE_REF}:22-JDK
+GraalVM 17-JDK|${IMAGE_REF}:17-JDK
+GraalVM 11-EE|${IMAGE_REF}:11-EE
+GraalVM 11|${IMAGE_REF}:11
+GraalVM 17|${IMAGE_REF}:17
+GraalVM 21-JDK|${IMAGE_REF}:21-JDK
+GraalVM 19|${IMAGE_REF}:19
+GraalVM 17-EE|${IMAGE_REF}:17-EE
+GraalVM 19-EE|${IMAGE_REF}:19-EE
+GraalVM 20-JDK|${IMAGE_REF}:20-JDK
+GraalVM 22-OC|${IMAGE_REF}:22-OC
+GraalVM 25-OC|${IMAGE_REF}:25-OC
+GraalVM 23-OC|${IMAGE_REF}:23-OC
+Temurin 22|${IMAGE_REF}:22-temurin
+Temurin 21|${IMAGE_REF}:21-temurin
+Temurin 23|${IMAGE_REF}:23-temurin
 
 " > /dev/null
